b482a2770a8ea3cedb6b1461e9648ee405d42614
Aleksey Kvashnin <aleks272007@gmail.com>
2024-06-26 10:39:01 +0400

    Изменил файл

--- StudentGroupService.java
+++ StudentGroupService.java
@@ -2,22 +2,39 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// Интерфейс для стратегии сортировки
+interface SortingStrategy<T> {
+    List<T> sort(List<T> items);
+}
+
+// Реализация сортировки по ID
+class SortByID implements SortingStrategy<Student> {
+    @Override
+    public List<Student> sort(List<Student> students) {
+        List<Student> sortedList = new ArrayList<>(students);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+}
+
+// Реализация сортировки по FIO
+class SortByFIO implements SortingStrategy<Student> {
+    @Override
+    public List<Student> sort(List<Student> students) {
+        List<Student> sortedList = new ArrayList<>(students);
+        sortedList.sort(new UserComparator<>());
+        return sortedList;
+    }
+}
+
 public class StudentGroupService {
-    private StudentGroup studentGroup;
+    private final StudentGroup studentGroup;
 
     public StudentGroupService(StudentGroup studentGroup) {
         this.studentGroup = studentGroup;
     }
 
-    public List<Student>getSortedStudentGroup(){
-        List<Student>studentsList = new ArrayList<>(studentGroup.getStudentList());
-        Collections.sort(studentsList);
-        return studentsList;
-    }
-
-    public List<Student> getSortedByFIO(){
-        List<Student>studentsList = new ArrayList<>(studentGroup.getStudentList());
-        studentsList.sort(new UserComparator<Student>());
-        return studentsList;
+    public List<Student> getSortedStudentGroup(SortingStrategy<Student> strategy) {
+        return strategy.sort(studentGroup.getStudentList());
     }
 }
